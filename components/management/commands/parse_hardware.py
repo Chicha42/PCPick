ba3cd6deddd17bd143ae281636aefc8c69c8b7a3
Chicha42 <ahm.fantazer@gmail.com>
2025-12-27 18:33:39 +0500

    Parsing has been improved, now parsing more elements and eliminating server-side components. The component selection algorithm has also been improved

--- components/management/commands/parse_hardware.py
+++ components/management/commands/parse_hardware.py
@@ -1,3 +1,5 @@
+import time
+
 from django.core.management import BaseCommand
 import requests
 from bs4 import BeautifulSoup
@@ -7,6 +9,15 @@ from components.models import CPU, GPU, Motherboard, RAM, PriceHistory
 class Command(BaseCommand):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36'}
     base_url = 'https://www.regard.ru/'
+    STOP_WORDS = [
+        'threadripper', 'xeon', 'epyc', 'quadro', 'tesla',
+        'nvidia a', 'rtx a', 'ecc', 'registered', 'server', 'workstation', 'sodimm',
+        'industrial', 'vPro', 'rdimm', 'buff', 'registered'
+    ]
+
+    def is_valid(self, name):
+        """Вспомогательный метод для проверки наличия стоп-слов в названии"""
+        return not any(word in name.lower() for word in self.STOP_WORDS)
 
     def get_items(self, url):
         """Вспомогательный метод для получения списка карточек"""
@@ -37,123 +48,150 @@ class Command(BaseCommand):
         self.parse_motherboards()
 
     def parse_cpus(self):
-        url = 'https://www.regard.ru/catalog/1001/processory'
-        items = self.get_items(url)
         cpus_tbody = self.get_tbody('https://www.cpubenchmark.net/cpu_list.php')
+        for page in range(1,6):
+            url = f'https://www.regard.ru/catalog/1001/processory?page={page}'
+            items = self.get_items(url)
 
-        for item in items:
-            url_name = item.find('a', class_='CardText_link__C_fPZ')
-            name = ' '.join(url_name.text.split()[1:-1]).replace(' - ', '-')
-            url = self.base_url + url_name.attrs['href']
-            socket = item.find('p', class_='CardText_text__fZPl_').text.split(',')[0]
-            price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
-            price = float(''.join(price))
-            benchmark_score = self.get_benchmark_score(cpus_tbody, name)
-            cpu_obj, created = CPU.objects.update_or_create(url=url, defaults={'socket': socket, 'benchmark_score': benchmark_score, 'price': price, 'name': name})
-            PriceHistory.objects.create(component_id=cpu_obj.id, component_type='cpu', price=price)
+            for item in items:
+                url_name = item.find('a', class_='CardText_link__C_fPZ')
+                name = ' '.join(url_name.text.split()[1:-1]).replace(' - ', '-')
+
+                if not self.is_valid(name):
+                    continue
+
+                url = self.base_url + url_name.attrs['href']
+                socket = item.find('p', class_='CardText_text__fZPl_').text.split(',')[0]
+                price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
+                price = float(''.join(price))
+                benchmark_score = self.get_benchmark_score(cpus_tbody, name)
+                cpu_obj, created = CPU.objects.update_or_create(url=url, defaults={'socket': socket, 'benchmark_score': benchmark_score, 'price': price, 'name': name})
+                PriceHistory.objects.create(component_id=cpu_obj.id, component_type='cpu', price=price)
+            time.sleep(1.5)
 
     def parse_gpus(self):
-        url = 'https://www.regard.ru/catalog/1013/videokarty'
-        items = self.get_items(url)
         manufacturers = ["Acer", "AFOX", "ASRock", "ASUS", "Biostar", "CBR", "Gigabyte", "HP", "INNO3D", "Maxsun", "MSI", "Palit", "PNY", "Sapphire", "Zotac"]
         pcie_versions = ['5.0', '4.0']
         gpus_tbody = self.get_tbody('https://www.videocardbenchmark.net/gpu_list.php')
 
-        for item in items:
-            url_name = item.find('a', class_='CardText_link__C_fPZ')
+        for page in range(1,6):
+            url = f'https://www.regard.ru/catalog/1013/videokarty?page={page}'
+            items = self.get_items(url)
 
-            name = ''
-            for manufacturer in manufacturers:
-                name = url_name.text
-                if manufacturer in name:
-                    name = ' '.join(name.split(f" {manufacturer}")[0].split()[2:])
-                    break
+            for item in items:
+                url_name = item.find('a', class_='CardText_link__C_fPZ')
 
-            url = self.base_url + url_name.attrs['href']
-            description = item.find('p', class_='CardText_text__fZPl_').text
+                name = ''
+                for manufacturer in manufacturers:
+                    name = url_name.text
+                    if manufacturer in name:
+                        name = ' '.join(name.split(f" {manufacturer}")[0].split()[2:])
+                        break
 
-            pcie_version = 0.0
-            for pcie in pcie_versions:
-                if pcie in description:
-                    pcie_version = float(pcie)
+                if not self.is_valid(name):
+                    continue
 
-            price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
-            price = float(''.join(price))
-            benchmark_score = self.get_benchmark_score(gpus_tbody, name)
-            gpu_obj, created = GPU.objects.update_or_create(url=url, defaults={'pcie_version': pcie_version, 'benchmark_score': benchmark_score,'price':price ,'name': name})
-            PriceHistory.objects.create(component_id=gpu_obj.id, component_type='gpu', price=price)
+                url = self.base_url + url_name.attrs['href']
+                description = item.find('p', class_='CardText_text__fZPl_').text
+
+                pcie_version = 0.0
+                for pcie in pcie_versions:
+                    if pcie in description:
+                        pcie_version = float(pcie)
+
+                price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
+                price = float(''.join(price))
+                benchmark_score = self.get_benchmark_score(gpus_tbody, name)
+                gpu_obj, created = GPU.objects.update_or_create(url=url, defaults={'pcie_version': pcie_version, 'benchmark_score': benchmark_score,'price':price ,'name': name})
+                PriceHistory.objects.create(component_id=gpu_obj.id, component_type='gpu', price=price)
+            time.sleep(1.5)
 
     def parse_ram(self):
-        url = 'https://www.regard.ru/catalog/1010/operativnaya-pamyat'
-        items = self.get_items(url)
         ram_types = ['DDR5', 'DDR4', 'DDR3', 'DDR2']
         ram_bar_counts = {'2 модуля': 2, '4 модуля': 4}
 
-        for item in items:
-            url_name = item.find('a', class_='CardText_link__C_fPZ')
-            name = url_name.text.split(' (')[0].split('z ')[-1]
-            url = self.base_url + url_name.attrs['href']
-            description = item.find('p', class_='CardText_text__fZPl_').text
-            ram_bar_count = ''
+        for page in range(1,6):
+            url = f'https://www.regard.ru/catalog/1010/operativnaya-pamyat?page={page}'
+            items = self.get_items(url)
 
-            ram_type = ''
-            for t in ram_types:
-                if t in description:
-                    ram_type = t
-                    break
+            for item in items:
+                url_name = item.find('a', class_='CardText_link__C_fPZ')
+                name = url_name.text.split(' (')[0].split('z ')[-1]
 
-            ram_capacity = int(description.split()[0])
+                if not self.is_valid(name):
+                    continue
 
-            ram_bar_count = 1
-            for count in ram_bar_counts.keys():
-                if count in description:
-                    ram_bar_count = ram_bar_counts[count]
-                    break
+                url = self.base_url + url_name.attrs['href']
+                description = item.find('p', class_='CardText_text__fZPl_').text
+                ram_bar_count = ''
 
-            price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
-            price = float(''.join(price))
-            ram_obj, created = RAM.objects.update_or_create(url=url, defaults={'ram_type': ram_type, 'ram_capacity': ram_capacity, 'ram_bar_count': ram_bar_count,'price': price ,'name': name})
-            PriceHistory.objects.create(component_id=ram_obj.id, component_type='ram', price=price)
+                ram_type = ''
+                for t in ram_types:
+                    if t in description:
+                        ram_type = t
+                        break
+
+                ram_capacity = int(description.split()[0])
+
+                ram_bar_count = 1
+                for count in ram_bar_counts.keys():
+                    if count in description:
+                        ram_bar_count = ram_bar_counts[count]
+                        break
+
+                price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
+                price = float(''.join(price))
+                ram_obj, created = RAM.objects.update_or_create(url=url, defaults={'ram_type': ram_type, 'ram_capacity': ram_capacity, 'ram_bar_count': ram_bar_count,'price': price ,'name': name})
+                PriceHistory.objects.create(component_id=ram_obj.id, component_type='ram', price=price)
+            time.sleep(1.5)
 
     def parse_motherboards(self):
-        url = 'https://www.regard.ru/catalog/1000/materinskie-platy'
-        items = self.get_items(url)
         sockets = ["AM4", "AM5", "LGA 1200", "LGA 1700", "LGA 1851", "LGA 775"]
         ram_types = ['DDR5', 'DDR4', 'DDR3', 'DDR2']
         pcie_versions = ['5.0', '4.0']
         ram_slots_versions = ['4xDDR', '2xDDR']
 
-        for item in items:
-            url_name = item.find('a', class_='CardText_link__C_fPZ')
-            name = ' '.join(url_name.text.split()[2:-1])
-            url = self.base_url + url_name.attrs['href']
-            description = item.find('p', class_='CardText_text__fZPl_').text
+        for page in range(1,6):
+            url = f'https://www.regard.ru/catalog/1000/materinskie-platy?page={page}'
+            items = self.get_items(url)
 
-            socket = ''
-            for s in sockets:
-                if s in description:
-                    socket = s
-                    break
+            for item in items:
+                url_name = item.find('a', class_='CardText_link__C_fPZ')
+                name = ' '.join(url_name.text.split()[2:-1])
 
-            ram_type = ''
-            for r in ram_types:
-                if r in description:
-                    ram_type = r
-                    break
+                if not self.is_valid(name):
+                    continue
 
-            pcie_version = 0.0
-            for pcie in pcie_versions:
-                if pcie in description:
-                    pcie_version = float(pcie)
-                    break
 
-            ram_slots = 0
-            for rs in ram_slots_versions:
-                if rs in description:
-                    ram_slots = int(rs[0])
-                    break
+                url = self.base_url + url_name.attrs['href']
+                description = item.find('p', class_='CardText_text__fZPl_').text
 
-            price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
-            price = float(''.join(price))
-            mb_obj, created = Motherboard.objects.update_or_create(url=url, defaults={'socket': socket, 'ram_type': ram_type, 'pcie_version':pcie_version, 'ram_slots':ram_slots, 'price': price, 'name': name})
-            PriceHistory.objects.create(component_id=mb_obj.id, component_type='mb', price=price)
+                socket = ''
+                for s in sockets:
+                    if s in description:
+                        socket = s
+                        break
+
+                ram_type = ''
+                for r in ram_types:
+                    if r in description:
+                        ram_type = r
+                        break
+
+                pcie_version = 0.0
+                for pcie in pcie_versions:
+                    if pcie in description:
+                        pcie_version = float(pcie)
+                        break
+
+                ram_slots = 0
+                for rs in ram_slots_versions:
+                    if rs in description:
+                        ram_slots = int(rs[0])
+                        break
+
+                price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
+                price = float(''.join(price))
+                mb_obj, created = Motherboard.objects.update_or_create(url=url, defaults={'socket': socket, 'ram_type': ram_type, 'pcie_version':pcie_version, 'ram_slots':ram_slots, 'price': price, 'name': name})
+                PriceHistory.objects.create(component_id=mb_obj.id, component_type='mb', price=price)
+            time.sleep(1.5)
--- configurator/views.py
+++ configurator/views.py
@@ -13,33 +13,37 @@ def index(request):
 
         core_budget = total_budget * Decimal('0.80')
         gpu = GPU.objects.filter(price__lte=core_budget * Decimal('0.35')).order_by('-benchmark_score').first()
-        cpu = CPU.objects.filter(price__lte=core_budget * Decimal('0.15')).order_by('-benchmark_score').first()
 
-        if gpu and cpu:
-            remaining_for_platform = core_budget - (gpu.price + cpu.price)
+        if gpu:
+            cpu_candidates = CPU.objects.filter(price__lte=core_budget * Decimal('0.15')).order_by('-benchmark_score')[:10]
+            for cpu in cpu_candidates:
+                platform_budget = core_budget - (cpu.price + gpu.price)
+                if platform_budget <= 0:
+                    continue
 
-            mb = Motherboard.objects.filter(
-                socket=cpu.socket,
-                pcie_version__gte=gpu.pcie_version,
-                price__lte=remaining_for_platform * Decimal('0.25')
-            ).first()
+                mb = Motherboard.objects.filter(
+                    socket=cpu.socket,
+                    pcie_version__gte=gpu.pcie_version,
+                    price__lte=platform_budget * Decimal('0.35')
+                ).first()
 
-            if mb:
-                ram_budget = remaining_for_platform - mb.price
-                ram = RAM.objects.filter(
-                    ram_type=mb.ram_type,
-                    ram_bar_count__lte=mb.ram_slots,
-                    price__lte=ram_budget
-                ).order_by('-price').first()
+                if mb:
+                    ram_budget = platform_budget - mb.price
+                    ram = RAM.objects.filter(
+                        ram_type=mb.ram_type,
+                        ram_bar_count__lte=mb.ram_slots,
+                        price__lte=ram_budget
+                    ).order_by('-ram_capacity', '-price').first()
 
-                if ram:
-                    context['build'] = {
-                        'cpu': cpu,
-                        'gpu': gpu,
-                        'motherboard': mb,
-                        'ram': ram,
-                        'total_price': cpu.price + gpu.price + mb.price + ram.price
-                    }
+                    if ram:
+                        context['build'] = {
+                            'cpu': cpu,
+                            'gpu': gpu,
+                            'motherboard': mb,
+                            'ram': ram,
+                            'total_price': cpu.price + gpu.price + mb.price + ram.price
+                        }
+                        break
                 else:
                     context['error'] = 'Не удалось подобрать подходящую оперативную память.'
             else:
