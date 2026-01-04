ddea8a171542e328ec6e203729e0e888cbc76ecc
Chicha42 <ahm.fantazer@gmail.com>
2026-01-04 17:20:36 +0500

    complete hardware parser with multi-page support and optimized benchmark matching

--- components/management/commands/parse_hardware.py
+++ components/management/commands/parse_hardware.py
@@ -1,5 +1,4 @@
 import time
-
 from django.core.management import BaseCommand
 import requests
 from bs4 import BeautifulSoup
@@ -12,7 +11,7 @@ class Command(BaseCommand):
     STOP_WORDS = [
         'threadripper', 'xeon', 'epyc', 'quadro', 'tesla',
         'nvidia a', 'rtx a', 'ecc', 'registered', 'server', 'workstation', 'sodimm',
-        'industrial', 'vPro', 'rdimm', 'buff', 'registered'
+        'industrial', 'vPro', 'rdimm', 'buff', 'registered', 'box'
     ]
 
     def is_valid(self, name):
@@ -31,14 +30,47 @@ class Command(BaseCommand):
         soup = BeautifulSoup(response.text, 'html.parser')
         return soup.find_all('tr')
 
-    def get_benchmark_score(self, tbody, name):
-        """Вспомогательный метод для получения бенчмарка из videocardbenchmark.net и cpubenchmark.net"""
-        benchmark_score = 0
+    def create_bench_dict(self, tbody):
+        """Вспомогательный метод для создания словаря бенчмарков из videocardbenchmark.net и cpubenchmark.net"""
+        bench_dict = {}
+        stop_words = ['laptop', 'mobile', 'design']
+
         for row in tbody:
-            row_name = row.find('a').text if row.find('a') is not None else ''
-            if row_name.lower() == name.lower():
-                benchmark_score = int(row.find_all('td')[1].text.replace(',', ''))
+            cells = row.find_all('td')
+            if len(cells) < 2:
+                continue
+
+            link = cells[0].find('a')
+            if not link:
+                continue
+
+            row_name = link.text.strip()
+
+            if any(word in row_name.lower() for word in stop_words):
+                continue
+
+            score_text = cells[1].text.replace(',', '')
+
+            if score_text.isdigit():
+                bench_dict[row_name.lower()] = int(score_text)
+
+        return bench_dict
+
+    def get_benchmark_score(self, bench_dict, name):
+        """Вспомогательный метод для получения бенчмарка из словаря"""
+        benchmark_score = 0
+
+        name_lower = name.lower()
+
+        if name_lower in bench_dict:
+            benchmark_score = bench_dict[name_lower]
+            return benchmark_score
+
+        for key in bench_dict.keys():
+            if key in name_lower or name_lower in key:
+                benchmark_score = bench_dict[key]
                 break
+
         return benchmark_score
 
     def handle(self, *args, **options):
@@ -49,22 +81,23 @@ class Command(BaseCommand):
 
     def parse_cpus(self):
         cpus_tbody = self.get_tbody('https://www.cpubenchmark.net/cpu_list.php')
+        bench_dict = self.create_bench_dict(cpus_tbody)
         for page in range(1,6):
-            url = f'https://www.regard.ru/catalog/1001/processory?page={page}'
+            url = f'https://www.regard.ru/catalog/1001/processory?page={page}&q=eyJzb3J0IjpbIm9yZGVyQnlQcmljZSIsImRlc2MiXX0'
             items = self.get_items(url)
 
             for item in items:
                 url_name = item.find('a', class_='CardText_link__C_fPZ')
-                name = ' '.join(url_name.text.split()[1:-1]).replace(' - ', '-')
 
-                if not self.is_valid(name):
+                if not self.is_valid(url_name.text):
                     continue
 
+                name = ' '.join(url_name.text.split()[1:-1]).replace(' - ', '-')
                 url = self.base_url + url_name.attrs['href']
                 socket = item.find('p', class_='CardText_text__fZPl_').text.split(',')[0]
                 price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
                 price = float(''.join(price))
-                benchmark_score = self.get_benchmark_score(cpus_tbody, name)
+                benchmark_score = self.get_benchmark_score(bench_dict, name)
                 cpu_obj, created = CPU.objects.update_or_create(url=url, defaults={'socket': socket, 'benchmark_score': benchmark_score, 'price': price, 'name': name})
                 PriceHistory.objects.create(component_id=cpu_obj.id, component_type='cpu', price=price)
             time.sleep(1.5)
@@ -72,7 +105,9 @@ class Command(BaseCommand):
     def parse_gpus(self):
         manufacturers = ["Acer", "AFOX", "ASRock", "ASUS", "Biostar", "CBR", "Gigabyte", "HP", "INNO3D", "Maxsun", "MSI", "Palit", "PNY", "Sapphire", "Zotac"]
         pcie_versions = ['5.0', '4.0']
+        gb_counts = ['32Gb', '24Gb', '20Gb', '16Gb', '12Gb', '10Gb', '8Gb', '6Gb', '4Gb', '2Gb', '1Gb', '512Mb']
         gpus_tbody = self.get_tbody('https://www.videocardbenchmark.net/gpu_list.php')
+        bench_dict = self.create_bench_dict(gpus_tbody)
 
         for page in range(1,6):
             url = f'https://www.regard.ru/catalog/1013/videokarty?page={page}'
@@ -80,12 +115,16 @@ class Command(BaseCommand):
 
             for item in items:
                 url_name = item.find('a', class_='CardText_link__C_fPZ')
-
                 name = ''
                 for manufacturer in manufacturers:
                     name = url_name.text
                     if manufacturer in name:
                         name = ' '.join(name.split(f" {manufacturer}")[0].split()[2:])
+                        break
+
+                for gb in gb_counts:
+                    if gb in url_name.text:
+                        name += f" {gb}"
                         break
 
                 if not self.is_valid(name):
@@ -101,7 +140,7 @@ class Command(BaseCommand):
 
                 price = item.find('span', class_='Price_price__m2aSe').text.split()[:-1]
                 price = float(''.join(price))
-                benchmark_score = self.get_benchmark_score(gpus_tbody, name)
+                benchmark_score = self.get_benchmark_score(bench_dict, name)
                 gpu_obj, created = GPU.objects.update_or_create(url=url, defaults={'pcie_version': pcie_version, 'benchmark_score': benchmark_score,'price':price ,'name': name})
                 PriceHistory.objects.create(component_id=gpu_obj.id, component_type='gpu', price=price)
             time.sleep(1.5)
@@ -123,7 +162,6 @@ class Command(BaseCommand):
 
                 url = self.base_url + url_name.attrs['href']
                 description = item.find('p', class_='CardText_text__fZPl_').text
-                ram_bar_count = ''
 
                 ram_type = ''
                 for t in ram_types:
