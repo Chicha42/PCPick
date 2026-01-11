b1c7adbbd0574a642f25495893f38e6046a52faf
Chicha42 <ahm.fantazer@gmail.com>
2026-01-11 20:42:25 +0500

    add a design and main screen with PC build cards

--- configurator/urls.py
+++ configurator/urls.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.my_builds, name='profile'),
+    path('save-build/', views.save_build, name='save_build'),
+    path('profile/info/', views.profile_info, name='profile_info'),
+    path('build/<int:pk>/', views.build_detail, name='build_detail'),
+    path('build/delete/<int:pk>', views.delete_build, name='delete_build'),
+    path('profile/info/edit_profile', views.edit_profile, name='edit_profile'),
+    path('profile/info/change_password/', views.change_password, name='change_password'),
 ]
--- configurator/utils.py
+++ configurator/utils.py
@@ -5,21 +5,48 @@ from components.models import PriceHistory
 
 def get_graph(dates, prices):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=dates, y=prices, mode='lines + markers', line=dict(color='#a855f7', width=3), marker=dict(size=8, color='#ffffff', line=dict(color='#a855f7', width=2)), name='Цена'))
+    fig.add_trace(go.Scatter(x=dates,
+                             y=prices,
+                             mode='lines+markers',
+                             line=dict(color='#a855f7', width=3),
+                             marker=dict(size=8, color='#ffffff', line=dict(color='#a855f7', width=2)),
+                             name='Цена',
+                             hovertemplate='%{y} ₽<extra></extra>'))
+
     fig.update_layout(template='plotly_dark',
                       paper_bgcolor='rgba(0,0,0,0)',
-                      plot_bgcolor='rgba(0,0,0,0)', margin=dict(l=10, r=10, b=10, t=10),
+                      plot_bgcolor='rgba(0,0,0,0)',
+                      margin=dict(l=10, r=10, b=30, t=10),
                       height=200,
                       showlegend=False,
-                      autosize=True)
-    return pio.to_html(fig, full_html=False, include_plotlyjs='cdn')
+                      autosize=True,
+                      dragmode=False,
+                      xaxis=dict(
+                          fixedrange=True,
+                          showgrid=False,
+                          tickfont=dict(size=10)
+                      ),
+                      yaxis=dict(
+                          fixedrange=True,
+                          showgrid=True,
+                          gridcolor='#2d3748',
+                          tickformat=".1f",
+                          ticksuffix=" тыс. ₽",
+                          tickfont=dict(size=10)))
 
-def get_component_chart(component_id, component_type):
+    config = {
+        'displayModeBar': False,
+        'responsive': True
+    }
+
+    return pio.to_html(fig, full_html=False, include_plotlyjs='cdn', config=config)
+
+def get_component_graph(component_id, component_type):
     history = PriceHistory.objects.filter(component_id=component_id, component_type=component_type).order_by('date_checked')
 
     if not history.exists():
         return None
 
     dates = [h.date_checked.strftime("%d.%m") for h in history]
-    prices = [float(h.price) for h in history]
+    prices = [float(h.price) / 1000 for h in history]
     return get_graph(dates, prices)
