3635105400c651bddbcdd2407be70520d8b0e65f
Chicha42 <ahm.fantazer@gmail.com>
2026-01-20 02:45:09 +0500

    add list_display, search_fields and list_filter in admin panel

--- components/admin.py
+++ components/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from .models import CPU, GPU, RAM, Motherboard, PriceHistory
 
-admin.site.register(CPU)
-admin.site.register(GPU)
-admin.site.register(RAM)
-admin.site.register(Motherboard)
-admin.site.register(PriceHistory)
+@admin.register(CPU)
+class CPUAdmin(admin.ModelAdmin):
+    list_display = ('name', 'socket', 'price', 'benchmark_score')
+    search_fields = ('name', 'socket')
+
+@admin.register(GPU)
+class GPUAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pcie_version', 'price', 'benchmark_score')
+    search_fields = ('name',)
+
+@admin.register(RAM)
+class RAMAdmin(admin.ModelAdmin):
+    list_display = ('name', 'ram_type', 'ram_capacity', 'ram_bar_count', 'price')
+    search_fields = ('name', 'ram_type')
+
+@admin.register(Motherboard)
+class MotherboardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'socket', 'ram_type', 'pcie_version', 'ram_slots', 'price')
+    search_fields = ('name', 'socket')
+
+@admin.register(PriceHistory)
+class PriceHistoryAdmin(admin.ModelAdmin):
+    list_display = ( 'component_id', 'component_type', 'price', 'date_checked')
+    list_filter = ('component_type', 'date_checked')
--- configurator/admin.py
+++ configurator/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from configurator.models import Build
+from .models import Build
 
-admin.site.register(Build)
+@admin.register(Build)
+class BuildAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cpu', 'gpu', 'ram', 'mb', 'user', 'total_price', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__username', 'id')
